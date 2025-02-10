7c8cab0f9380e4ebc80fcf87227633b59a88a2cf
Denis <fpv.airplane.barnaul@gmail.com>
2025-02-11 00:00:40 +0300

    Initial Commit

---
+++ AvitoInternship/AvitoInternshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvitoInternshipApp.swift
+//  AvitoInternship
+//
+//  Created by Denis on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AvitoInternshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
